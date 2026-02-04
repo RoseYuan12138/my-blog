83a26008e5f7b4709c12756318f9445f1e5cf3c5
RoseYuan12138 <yuanxinhang200211@gmail.com>
2026-02-04 00:22:53 -0800

    Quartz sync: Feb 4, 2026, 12:22 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "RoseYuan12138.github.io",
+    baseUrl: "RoseYuan12138.github.io/my-blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
